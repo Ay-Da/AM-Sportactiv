7a870e80a89e35c39d9c393e87f2f0a887aab01c
Aymen Daboussi <93875562+Ay-Da@users.noreply.github.com>
2022-11-17 20:45:19 +0100

    Add files via upload

---
+++ customer/pay_offline.php
@@ -0,0 +1,53 @@
+<center><!-- center Starts -->
+
+<h1> Pay OffLine Methods  </h1>
+
+<p class="text-muted" >
+
+If you have any questions, please feel free to <a href="../contact.php" >contact us,</a> our customer service center is working for you 24/7.
+
+</p>
+
+</center><!-- center Ends -->
+
+<hr>
+
+
+<div class="table-responsive" ><!-- table-responsive Starts -->
+
+<table class="table table-bordered table-hover table-striped" ><!-- table table-bordered table-hover table-striped Starts -->
+
+<thead><!-- thead Starts -->
+
+<tr>
+
+<th> Bank Account Details </th>
+
+<th> Mobile Payment </th>
+
+<th> Western Union Details </th>
+
+</tr>
+
+</thead><!-- thead Ends -->
+
+<tbody><!-- tbody Starts -->
+
+<tr>
+
+<td> Bank Name: Sparkasse <br> Account No: 00000000 <br> Branch Code: 0342 <br> Branch Name: NRV </td>
+
+<td> Mobile No: 017745*** <br> Name: ***** </td>
+
+<td> Full Name: ***** <br> Mobile No: 017745*** <br> Country: Deutchland
+</td>
+
+
+</tr>
+
+</tbody><!-- tbody Ends -->
+
+
+</table><!-- table table-bordered table-hover table-striped Ends -->
+
+</div><!-- table-responsive Ends -->
